c994f5dcdc4e43930014d245e02121278ea91f2f
Eda Kaş <61595808+edakass@users.noreply.github.com>
2022-04-06 17:15:03 +0300

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WriteDataToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Dosyaya Veri Yazma için C# da 4 adet yöntem var
+             * 1.Yöntem:
+             * Dizi elemanlarını satır satır text dosyasına yazdırma
+             */
+            string[] rows = { "Number:1", "Name: Harry", "Surname: Styles" };
+            System.IO.File.WriteAllLines(@"C:\Users\eda\Desktop\csharp_examples\write.txt",rows);
+            
+        }
+    }
+}
