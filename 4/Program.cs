f60b7418a42d6d0e08f10ebfda565146420fb285
Eda Kaş <61595808+edakass@users.noreply.github.com>
2022-04-06 20:42:04 +0300

    Add files via upload

---
+++ 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WriteDataToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Dosyaya Veri Yazma için C# da 4 adet yöntem var
+             * 4.Yöntem:
+             * Varolan bir text dosyasına metin ekleme.
+             * Metin dosyasının içeriği silinmeden yeni ekleme
+             */
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\eda\Desktop\csharp_examples\write4.txt", true))
+                //bu true parametresi sayesinde veri ekleyebileceğiz,bunu eklemezsek
+                //varsayılan olarak false atanır o da override işlemi yapar,yani dosyayı siler üzerine yazar
+                file.WriteLine("Hello World");
+
+        }
+    }
+}
